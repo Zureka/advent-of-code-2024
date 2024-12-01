fb7e6ce9364a371cda9a9a1dfb1994b063d788ce
Alex Zurek <alexander.j.zurek@gmail.com>
2024-12-01 07:47:33 -0500

    Elixir - Day 1 both parts

--- elixir/lib/day01.ex
+++ elixir/lib/day01.ex
@@ -1,10 +1,55 @@
 defmodule Day01.Part1 do
-  def solve(_input) do
+  def solve(input) do
+    [left, right] = createSublists(input)
+
+    Enum.zip(left, right)
+    |> Enum.reduce(0, fn {a, b}, acc -> acc + abs(b - a) end)
+  end
+
+  defp createSublists(input) do
+    [left, right] =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.reduce([[], []], fn line, [left, right] ->
+        [a, b] =
+          String.split(line, "   ", trim: true)
+          |> Enum.map(&String.to_integer/1)
+
+        [[a | left], [b | right]]
+      end)
+
+    [Enum.sort(left), Enum.sort(right)]
   end
 end
 
 defmodule Day01.Part2 do
-  def solve(_input) do
+  def solve(input) do
+    [left, right] = createSublists(input)
+
+    left
+    |> Enum.reduce(0, fn x, total ->
+      count =
+        Enum.reduce(right, 0, fn y, acc ->
+          if x == y, do: acc + 1, else: acc
+        end)
+
+      total + x * count
+    end)
+  end
+
+  defp createSublists(input) do
+    [left, right] =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.reduce([[], []], fn line, [left, right] ->
+        [a, b] =
+          String.split(line, "   ", trim: true)
+          |> Enum.map(&String.to_integer/1)
+
+        [[a | left], [b | right]]
+      end)
+
+    [Enum.sort(left), Enum.sort(right)]
   end
 end
 
@@ -15,7 +60,7 @@ defmodule Mix.Tasks.Day01 do
     {:ok, input} = File.read("inputs/day01-input.txt")
 
     IO.puts("--- Part 1 ---")
-    IO.puts(Day01.Part1.solve(input))
+    IO.inspect(Day01.Part1.solve(input))
     IO.puts("")
     IO.puts("--- Part 2 ---")
     IO.puts(Day01.Part2.solve(input))
