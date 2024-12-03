51a9a4d69de713d4c106b7a9edb0d8b8cc65c914
Alex Zurek <alexander.j.zurek@gmail.com>
2024-12-03 08:00:52 -0500

    Part 1 completed

--- golang/day03/main.go
+++ golang/day03/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	filename := "input.txt"
+	part1(filename)
+}
+
+func part1(filename string) {
+	matches := processInput(filename)
+	r := regexp.MustCompile(`\d{1,3}`)
+	answer := 0
+
+	for _, match := range matches {
+		digits := r.FindAllString(match, -1)
+		a, _ := strconv.Atoi(digits[0])
+		b, _ := strconv.Atoi(digits[1])
+
+		answer += a * b
+	}
+
+	fmt.Printf("Part 1: %d\n", answer)
+}
+
+func processInput(filename string) []string {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		message := fmt.Sprintf("Error reading file: %v\n", err)
+		panic(message)
+	}
+
+	r := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	return r.FindAllString(string(bytes), -1)
 }
