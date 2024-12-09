83de28ff8e17b27f364de7041eb0c951f7101652
Alex Zurek <alexander.j.zurek@gmail.com>
2024-12-09 07:14:53 -0500

    Finished Part 2

--- golang/day08/main.go
+++ golang/day08/main.go
@@ -44,8 +44,48 @@ func part1(filename string) {
 }
 
 func part2(filename string) {
-	processInput(filename)
-	answer := 0
+	antennas, numRows, numColumns := processInput(filename)
+
+	antinodes := [][]int{}
+	seen := map[string]bool{}
+
+	for _, frequency := range antennas {
+		for i, v1 := range frequency {
+			for j, v2 := range frequency {
+				if i != j {
+					outOfBounds := false
+					xSlope := v2[0] - v1[0]
+					ySlope := v2[1] - v1[1]
+					previousX := v2[0]
+					previousY := v2[1]
+
+					for !outOfBounds {
+						antinode := []int{(previousX + xSlope), (previousY + ySlope)}
+						if antinode[0] >= 0 && antinode[0] < numColumns && antinode[1] >= 0 && antinode[1] < numRows {
+							key := fmt.Sprintf("%d,%d", antinode[0], antinode[1])
+							if !seen[key] {
+								seen[key] = true
+								antinodes = append(antinodes, antinode)
+							}
+							previousX = antinode[0]
+							previousY = antinode[1]
+						} else {
+							outOfBounds = true
+						}
+					}
+				}
+				key := fmt.Sprintf("%d,%d", v1[0], v1[1])
+				if !seen[key] {
+					seen[key] = true
+					antinode := []int{v1[0], v1[1]}
+					antinodes = append(antinodes, antinode)
+				}
+			}
+		}
+	}
+
+	answer := len(antinodes)
+
 	fmt.Printf("Part 2: %d\n", answer)
 }
 
