4c7181769052d95a55b03751ddb0b90ab869767a
Alex Zurek <alexander.j.zurek@gmail.com>
2024-12-15 18:08:32 -0500

    Setup for day 14

---
+++ golang/day14/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	filename := "test.txt"
+	part1(filename)
+	part2(filename)
+}
+
+func part1(filename string) {
+	answer := 0
+	fmt.Printf("Part 1: %d\n", answer)
+}
+
+func part2(filename string) {
+	answer := 0
+	fmt.Printf("Part 2: %d\n", answer)
+}
